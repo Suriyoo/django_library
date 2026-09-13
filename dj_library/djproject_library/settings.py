8ca7327ce778ba1b1e803251f8a4c14e15b70306
Suri <77195466+Suriyoo@users.noreply.github.com>
2026-09-13 10:19:22 +0800

    Update database settings to use dj_database_url
    Replaced default SQLite database configuration with dj_database_url for better flexibility.

--- dj_library/djproject_library/settings.py
+++ dj_library/djproject_library/settings.py
@@ -84,11 +84,18 @@ WSGI_APPLICATION = 'djproject_library.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}'
+    )
 }
 
 
